538e35571fa624c489eea10d6995f70e1e194d4b
Esso14 <e.tchatokey@gmail.com>
2026-06-12 22:28:16 +0200

    feat: implement config

---
+++ config.py
@@ -0,0 +1,23 @@
+import json
+from logger import logger
+
+class ConfigManager: 
+
+    def __init__(self, config_path: str):
+        self.config_path = config_path
+        self.config = self._load()
+
+    def _load(self) -> dict:
+        with open(self.config_path, "r", encoding="utf-8") as file:
+            config_data = json.load(file)
+            logger.info(f"Configuration file is loaded")
+            return config_data
+        
+    #--- Decorator: transforme methode in variable ----   
+    @property
+    def config(self):
+        return self._config
+        
+    def get(self, key, default=None):
+        return self._config.get(key, default)
+
